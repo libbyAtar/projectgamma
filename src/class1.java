92046c77807ae6a0bee043aa936aa58a94e6b44b
Berf21 <berfinatess21@gmail.com>
2022-03-26 19:41:49 -0400

    new 2

---
+++ src/class1.java
@@ -0,0 +1,17 @@
+public class class1 {
+    public static void main(String[] args) {
+        System.out.println(new StringBuilder("java").reverse());
+    }
+}
+
+
+/**
+ *
+ * echo "# projectgamma" >> README.md
+ * git init
+ * git add README.md
+ * git commit -m "first commit"
+ * git branch -M main
+ * git remote add origin https://github.com/Berf21/projectgamma.git
+ * git push -u origin main
+ */
